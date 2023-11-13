0cbdc4ec1ce3c8950fe1b526f38e007891709a4f
Valentin Duffet <vavadu07@gmail.com>
2023-11-13 10:27:08 +0100

    add torbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TopbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
